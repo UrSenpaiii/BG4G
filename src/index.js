d3570c7369f75e258f0561b45519eb5b2fc95e8e
FiNaLiZee <sd103e@mail.ru>
2020-02-03 01:14:36 +0300

    Auto init of vue comps

--- src/index.js
+++ src/index.js
@@ -16,4 +16,17 @@ import './js/'
 import './assets/css/main.css'
 
 // Vue.js
-import './components/init'
+window.Vue = require('vue');
+
+const prefix = "v-";
+const components = require.context("./", true, /\w+\.(vue)$/);
+components.keys().forEach(filename => {
+  const config = components(filename);
+  const name = filename.split("/").pop().replace(/\.\w+$/, "").replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
+  Vue.component(prefix + name, config.default || config);
+  console.log(filename, "mapped by", prefix + name);
+});
+
+const app = new Vue({
+  el: '#app',
+});
